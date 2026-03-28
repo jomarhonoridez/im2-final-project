647f4f0be8292f7b30ed67cd09e67b57524510d9
Jomar Honoridez <jomarhonoridez@gmail.com>
2026-03-28 07:56:45 +0000

    update login page

--- login.php
+++ login.php
@@ -1,22 +1,26 @@
-<?php include'views/header.php';?>
+<?php include 'views/header.php'; ?>
 
-<div class="input-form">
-    <p class="title">LOGIN</p>
-            <form action="models/login_account.php" method="POST">
-               <?php
-                    if(isset($_GET['err'])){
-                        echo"<p class=\"alert\">";
-                        echo"Invalid email or password";
-                        echo"</p>";
-                    }
-               ?>
-                <label for="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Bruce@gmail.com">
-                    
-                <label for="password">Password</label>
-                     <input type="password" id="password" name="password" placeholder="********">
-                     <input type="submit" value="Log in">
-            </form>
+<div class="container" style="display:flex; align-items:center; justify-items:center">
+
+    <div class="input-form">
+        <p class="title">LOGIN</p>
+        <form action="models/login_account.php" method="POST">
+            <?php
+            if (isset($_GET['err'])) {
+                echo "<p class=\"alert\">";
+                echo "Invalid email or password";
+                echo "</p>";
+            }
+            ?>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Bruce@gmail.com">
+
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="********">
+            <input type="submit" value="Log in">
+        </form>
+    </div>
 </div>
 
-<?php include'views/footer.php'; 
+
+<?php include 'views/footer.php';
