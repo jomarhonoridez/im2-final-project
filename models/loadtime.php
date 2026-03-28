38bf003d986e9b2f23b3f849cad564bf58dc41a6
Jomar Honoridez <jomarhonoridez@gmail.com>
2026-03-28 07:48:48 +0000

    update posts style

--- models/loadtime.php
+++ models/loadtime.php
@@ -1,28 +1,35 @@
-
 <?php
 include 'db/dbconnect.php';
-$aid=$_SESSION['aid'];
-$sql = "SELECT ACCOUNT .aid, "
-        . "ACCOUNT.firstname,"
-        . " post.pid,"
-        . " post.post_text,"
-        . " post.post_date,"
-        . " post.post_time "
-        . " FROM ACCOUNT INNER JOIN post "
-        . " ON ACCOUNT .aid = post.aid;";
+$aid = $_SESSION['aid'];
+$sql = "SELECT account.aid, "
+  . "account.firstname,"
+  . " post.pid,"
+  . " post.post_text,"
+  . " post.post_date,"
+  . " post.post_time "
+  . " FROM account INNER JOIN post "
+  . " ON account .aid = post.aid;";
 
-$result=$con->query($sql);
+$result = $con->query($sql);
 
-while ($row= mysqli_fetch_assoc($result)){
-    echo"<div class=\"post-block\">";
-     echo"<div class='post-name'>";
-      echo $row['firstname']. " ";
-    echo $row['post_text'];
-    echo "</div>";
-    
-    
-     echo"<div class='post-content'>";
-      echo $row['post_text'];
-    echo "</div>";
-    echo "</div>";
+if (!$result) {
+  echo "failed to fetch posts";
+  return;
 }
+
+while ($row = mysqli_fetch_assoc($result)) {
+?>
+  <div class="post-block">
+    <div class="post-card">
+      <div class="post-name"><?php echo $row['firstname'] ?></div>
+      <div class="post-timestamp">
+        <span><?php echo $row['post_date'] ?></span>
+        <span><?php echo $row['post_time'] ?></span>
+      </div>
+      <div class="post-content"><?php echo $row['post_text'] ?></div>
+    </div>
+
+  </div>
+<?php
+}
+?>
