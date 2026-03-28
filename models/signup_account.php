1b70a8c4987f472182508c7f5403d045356a0992
Jomar Honoridez <jomarhonoridez@gmail.com>
2026-03-28 08:11:25 +0000

    fix issue not able to add new account

--- models/signup_account.php
+++ models/signup_account.php
@@ -8,6 +8,12 @@ $ln = htmlspecialchars( $_POST ['lastname']);
 $pass = htmlspecialchars( $_POST ['password']);
 $cpass = htmlspecialchars( $_POST ['cpassword']);
 
-$sql = "INSERT INTO account VALUES (0, '$email', '$fn', '$ln', '$pass')";
+$sql = "INSERT INTO account (email, firstname, lastname, password) VALUES ('$email', '$fn', '$ln', '$pass')";
 
-$con->query($sql);
+$result = $con->query($sql);
+if (!$result) {
+    echo "failed to signup";
+}
+?>
+
+<a href="/login.php">back to home</a>
